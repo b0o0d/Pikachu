f533c11f57e8615fa9b64ffa99195a96194906f6
_bo0od_ <bobson8894@gmail.com>
2024-09-07 17:38:42 +0800

    Initial Commit

---
+++ Pikachu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pikachu
+//
+//  Created by Bo Chiu on 2024/9/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pikachu/PikachuApp.swift
@@ -0,0 +1,17 @@
+//
+//  PikachuApp.swift
+//  Pikachu
+//
+//  Created by Bo Chiu on 2024/9/7.
+//
+
+import SwiftUI
+
+@main
+struct PikachuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
